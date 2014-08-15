29181aead172a7b4412a994009c67afe996abba6
ishanagr <ishan.agrawal90@gmail.com>
2014-08-15 19:10:35 +0800

    Updated api

--- ethnicity.py
+++ ethnicity.py
@@ -3,6 +3,7 @@ app = Flask(__name__)
 app.debug = True
 
 import database
+import operator
 import json
 from models import *
 
@@ -19,11 +20,11 @@ def ethnicity_search():
 	last_name = request.args.get('last_name')
 	result = db.session.query(Name).filter_by(lastname=last_name).all()
 	if len(result) != 0:
-		hispanic =  result[0].hispanic
-		asian =  result[0].asian
-		white =  result[0].white
-		african =  result[0].african
-		res = {'ethnicity': 'asian', 'probability': asian};
+		ethnicitage = {'hispanic':result[0].hispanic, 'asian':result[0].asian, 'white':result[0].white, 'african':result[0].african}
+		print ethnicitage
+		top = sorted(ethnicitage.iteritems(), key=operator.itemgetter(1))
+		top.reverse()
+		res = {'ethnicity': top[0][0], 'probability': top[0][1]};
 	else:
 		res = run_model()
 	#check in names db and return the result, otherwise call other function
